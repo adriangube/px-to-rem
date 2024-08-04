603c8cd9acc9fab592d0a77bc78af8734491a583
adriangube <adrianguillenbermudez@gmail.com>
2024-08-04 13:50:27 +0100

    feat: style converter-layout component
    Co-authored-by: Pedro Barquín <PedroBarquin@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-
+import './reset.css'
 
 export const metadata: Metadata = {
   title: 'PX to REM Calculator',
--- src/components/converter-layout/converter-layout.tsx
+++ src/components/converter-layout/converter-layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './converter-layout.module.css'
 import { useRouter } from 'next/navigation'
+import ReloadIcon from '@/assets/reload.svg'
+import Image from 'next/image'
 
 export type ConverterLayoutProps = {
   firstUnit: string
@@ -36,28 +38,46 @@ export const ConverterLayout = ({
   return (
     <div className={styles['converter-layout__main']}>
       <h2>{firstUnitLabel} to {secondUnitLabel}</h2>
-      <input
-        type="text"
-        value={firstUnit}
-        placeholder={firstUnitLabel}
-        onChange={onFirstUnitChangeHandler}
-      />
-      <div onClick={onSwitchHandler}>Switch</div>
-      <input
-        type="text"
-        value={secondUnit}
-        placeholder={secondUnitLabel}
-        onChange={onSecondUnitChangeHandler}
-      />
-      <div>
-        <span>Default</span>
+      <div className={styles.row}>
+        <div className={styles['converter-layout_form-group']}>
+          <label htmlFor="first-unit-input">{firstUnitLabel}</label>
+          <input
+            className={styles.input}
+            name="first-unit-input"
+            type="text"
+            value={firstUnit}
+            placeholder={firstUnitLabel}
+            onChange={onFirstUnitChangeHandler}
+          />
+        </div>
+        <div className={styles['switcher-container']} onClick={onSwitchHandler}>
+          <Image
+            src={ReloadIcon}
+            alt="switch icon"
+          />
+        </div>
+        <div className={styles['converter-layout_form-group']}>
+          <label htmlFor="second-unit-input">{secondUnitLabel}</label>
+          <input
+            className={styles.input}
+            name="second-unit-input"
+            type="text"
+            value={secondUnit}
+            placeholder={secondUnitLabel}
+            onChange={onSecondUnitChangeHandler}
+          />
+        </div>
+      </div>
+      <div className={`${styles.row} ${styles['force-row']}`}>
+        <span className={styles.text}>Calculation based on a root font-size of </span>
         <input
+          className={`${styles.input} ${styles.small}`}
           type="text"
           value={defaultConversionUnit}
           onChange={onDefaultConversionUnitChangeHandler}
           placeholder={defaultConversionUnitLabel}
         />
-        <span>{defaultConversionUnitLabel}</span>
+        <span className={styles.text}>{defaultConversionUnitLabel}</span>
       </div>
     </div>
   )
